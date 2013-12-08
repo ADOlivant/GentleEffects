13e8e3f35d33f888f0102122378feff9ac3498f1
ADOlivant <24426@longroad.ac.uk>
2013-12-08 21:29:15 +0000

    Add Customer Window
    Including functionality for Reset button. Save button functionality to
    follow.

---
+++ addCustomer.py
@@ -0,0 +1,168 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtSql import *
+
+import sys
+import re
+
+class AddCustomer(QMainWindow):
+	"""Adding Customer data to SQL Database with PyQt4"""
+
+	def __init__(self):
+		super().__init__()
+
+		self.setWindowTitle("Add Customer Details | Gentle Effects CMS")
+
+		self.db = QSqlDatabase.addDatabase("QSQLITE")
+		self.db.setDatabaseName("GentleEffects.db")
+		self.db.open()
+
+		self.title_label = QLabel("""<html>
+                                          <body>
+                                               <p><span style=" font-size:16pt; font-weight:1000;">New Customer</span></p>
+                                          </body>
+                                     </html>""")
+
+		self.fName_label = QLabel("First Name(s)")
+		self.lName_label = QLabel("Last Name(s)")
+		self.dateofbirth_label = QLabel("Date of Birth")
+		self.number_label = QLabel("House Name / Number")
+		self.road_label = QLabel("Road")
+		self.city_label = QLabel("Town / City")
+		self.county_label = QLabel("County")
+		self.postcode_label = QLabel("Postcode")
+		self.mobile_label = QLabel("Mobile Number")
+		self.home_label = QLabel("Home Number")
+		self.email_label = QLabel("Email")
+		
+		#Preferred Contact
+		self.preferred_groupbox = QGroupBox("Preferred Contact")
+		self.mobile_radio = QRadioButton("Mobile")
+		self.home_radio = QRadioButton("Home")
+		self.preferred_layout = QVBoxLayout()
+		self.preferred_layout.addWidget(self.mobile_radio)
+		self.preferred_layout.addWidget(self.home_radio)
+		self.preferred_groupbox.setLayout(self.preferred_layout)
+
+		self.fName_lineedit = QLineEdit()
+		self.lName_lineedit = QLineEdit()
+		self.year_lineedit = QLineEdit("YYYY")
+		self.year_lineedit.setMaxLength(4)
+		self.year_lineedit.setFixedWidth(35)
+		self.month_lineedit = QLineEdit("MM")
+		self.month_lineedit.setMaxLength(2)
+		self.month_lineedit.setFixedWidth(25)
+		self.day_lineedit = QLineEdit("DD")
+		self.day_lineedit.setMaxLength(2)
+		self.day_lineedit.setFixedWidth(25)
+		self.number_lineedit = QLineEdit()
+		self.road_lineedit = QLineEdit()
+		self.city_lineedit = QLineEdit()
+		self.county_lineedit = QLineEdit()
+		self.postcode_lineedit = QLineEdit()
+		self.mobile_lineedit = QLineEdit()
+		self.home_lineedit = QLineEdit()
+		self.email_lineedit = QLineEdit()
+
+		self.slash_label = QLabel("/")
+		self.slash2_label = QLabel("/")
+
+		self.save_button = QPushButton("Save New Customer")
+		self.reset_button = QPushButton("Reset")
+
+		self.error_label = QLabel("Errors to Appear Here")
+
+		self.dateofbirth_layout = QHBoxLayout()
+		self.dateofbirth_layout.addWidget(self.year_lineedit)
+		self.dateofbirth_layout.addWidget(self.slash_label)
+		self.dateofbirth_layout.addWidget(self.month_lineedit)
+		self.dateofbirth_layout.addWidget(self.slash2_label)
+		self.dateofbirth_layout.addWidget(self.day_lineedit)
+		self.dateofbirth_widget = QWidget()
+		self.dateofbirth_widget.setLayout(self.dateofbirth_layout)
+
+		self.name_layout = QGridLayout()
+		self.name_layout.addWidget(self.fName_label,0,0)
+		self.name_layout.addWidget(self.fName_lineedit,0,1)
+		self.name_layout.addWidget(self.lName_label,1,0)
+		self.name_layout.addWidget(self.lName_lineedit,1,1)
+		self.name_layout.addWidget(self.dateofbirth_label,2,0)
+		self.name_layout.addWidget(self.dateofbirth_widget,2,1)
+		self.name_widget = QWidget()
+		self.name_widget.setLayout(self.name_layout)
+
+		self.address_layout = QGridLayout()
+		self.address_layout.addWidget(self.number_label,0,0)
+		self.address_layout.addWidget(self.number_lineedit,0,1)
+		self.address_layout.addWidget(self.road_label,1,0)
+		self.address_layout.addWidget(self.road_lineedit,1,1)
+		self.address_layout.addWidget(self.city_label,2,0)
+		self.address_layout.addWidget(self.city_lineedit,2,1)
+		self.address_layout.addWidget(self.county_label,3,0)
+		self.address_layout.addWidget(self.county_lineedit,3,1)
+		self.address_layout.addWidget(self.postcode_label,4,0)
+		self.address_layout.addWidget(self.postcode_lineedit,4,1)
+		self.address_widget = QWidget()
+		self.address_widget.setLayout(self.address_layout)
+
+		self.contact_layout = QGridLayout()
+		self.contact_layout.addWidget(self.mobile_label,0,0)
+		self.contact_layout.addWidget(self.mobile_lineedit,0,1)
+		self.contact_layout.addWidget(self.home_label,1,0)
+		self.contact_layout.addWidget(self.home_lineedit,1,1)
+		self.contact_layout.addWidget(self.preferred_groupbox,2,1)
+		self.contact_layout.addWidget(self.email_label,3,0)
+		self.contact_layout.addWidget(self.email_lineedit,3,1)
+		self.contact_widget = QWidget()
+		self.contact_widget.setLayout(self.contact_layout)
+
+		self.button_layout = QVBoxLayout()
+		self.button_layout.addWidget(self.save_button)
+		self.button_layout.addWidget(self.reset_button)
+		self.button_widget = QWidget()
+		self.button_widget.setLayout(self.button_layout)
+
+		self.data_layout = QGridLayout()
+		self.data_layout.addWidget(self.name_widget,0,0)
+		self.data_layout.addWidget(self.contact_widget,0,1)
+		#self.data_layout.addWidget(self.dateofbirth_widget,1,0)
+		self.data_layout.addWidget(self.address_widget,2,0)
+		self.data_layout.addWidget(self.button_widget)
+		self.data_widget = QWidget()
+		self.data_widget.setLayout(self.data_layout)
+
+		self.main_layout = QVBoxLayout()
+		self.main_layout.addWidget(self.title_label)
+		self.main_layout.addWidget(self.error_label)
+		self.main_layout.addWidget(self.data_widget)
+		self.main_widget = QWidget()
+		self.main_widget.setLayout(self.main_layout)
+
+		self.error_label.hide()
+
+		self.setCentralWidget(self.main_widget)
+
+		#connections
+		self.reset_button.clicked.connect(self.reset_data)
+
+	def reset_data(self):
+		self.fName_lineedit.clear()
+		self.lName_lineedit.clear()
+		self.year_lineedit.setText("YYYY")
+		self.month_lineedit.setText("MM")
+		self.day_lineedit.setText("DD")
+		self.number_lineedit.clear()
+		self.road_lineedit.clear()
+		self.city_lineedit.clear()
+		self.county_lineedit.clear()
+		self.postcode_lineedit.clear()
+		self.mobile_lineedit.clear()
+		self.home_lineedit.clear()
+		self.email_lineedit.clear()
+
+if __name__ == "__main__":
+    application = QApplication(sys.argv)
+    window = AddCustomer()
+    window.show()
+    window.raise_()
+    application.exec_()

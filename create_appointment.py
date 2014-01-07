109668f74cbbdb67e52cd47a7287a1b86a31f4b7
ADOlivant <24426@longroad.ac.uk>
2014-01-07 08:23:26 +0000

    Create Appointment - Layout Fix & Functionality Added

--- create_appointment.py
+++ create_appointment.py
@@ -20,14 +20,19 @@ class CreateAppointment(QWidget):
 				     </html>""")
 
         self.date_selector = QCalendarWidget()
+        self.date_selector.setEnabled(False)
 
         self.time_selector = QTimeEdit()
         self.time_selector.setDisplayFormat("HH:mm")
+        self.time_selector.setEnabled(False)
+
         self.treatment_label = QLabel("Treatment: ")
         self.create_treatment_model()
         self.treatment_combobox = QComboBox()
         self.treatment_combobox.setModel(self.model)
         self.treatment_combobox.setModelColumn(1)
+        self.select_treatment_button = QPushButton("Select")
+        
 
         self.duration_label = QLabel("Duration of Treatment: ")
         self.duration_time = QLabel()
@@ -35,16 +40,26 @@ class CreateAppointment(QWidget):
         self.duration = self.model.index(self.treatment_combobox.currentIndex(),3).data()
         self.duration_time.setText(self.duration)
 
-        self.layout = QGridLayout()
-        self.layout.addWidget(self.title_label,0,0,1,3)
-        self.layout.addWidget(self.date_selector,1,0)
-        self.layout.addWidget(self.time_selector,2,0)
-        self.layout.addWidget(self.treatment_label,1,1)
-        self.layout.addWidget(self.treatment_combobox,1,2)
-        self.layout.addWidget(self.duration_label,2,1)
-        self.layout.addWidget(self.duration_time,2,2)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.title_label)
+        self.layout.addWidget(self.date_selector)
+        self.layout.addWidget(self.time_selector)
+        self.layout.addWidget(self.treatment_label)
+        self.layout.addWidget(self.treatment_combobox)
+        self.layout.addWidget(self.select_treatment_button)
+        self.layout.addWidget(self.duration_label)
+        self.layout.addWidget(self.duration_time)
 
         self.setLayout(self.layout)
+
+        #connections
+        self.select_treatment_button.clicked.connect(self.enable_creation)
+
+    def enable_creation(self):
+        self.select_treatment_button.setEnabled(False)
+        self.treatment_combobox.setEnabled(False)
+        self.date_selector.setEnabled(True)
+        self.time_selector.setEnabled(True)
 
     def create_treatment_model(self):
         self.model = QSqlRelationalTableModel()
@@ -59,8 +74,8 @@ class CreateAppointment(QWidget):
         self.query.addBindValue(CustomerID)
         self.query.exec_()
         #Attribute Error - QSqlQuery object has no attribute select - check PyQt Docs.
-        self.customer_details = self.query.select()
-        print(self.customer_details)
-        return self.customer_details 
+        #self.customer_details = self.query.select()
+        #print(self.customer_details)
+        #return self.customer_details 
         
         

725e688f7ebfb0fad2dbdabbdde1b94dd2d40b53
Adam Olivant <24426@longroad.ac.uk>
2014-02-10 13:54:11 +0000

    Amend MediInfo Wgt. - Population & Update String

--- add_medical_information.py
+++ add_medical_information.py
@@ -93,7 +93,7 @@ class AddMedicalInfo(QWidget):
         #For Data Save
         self.date_time_stamp_save = self.date_time_stamp.strftime("%Y-%m-%d %H:%M")
         #For Display
-        self.date_time_stamp_display = self.date_time_stamp.strftime("%Y-%m-%d %H:%M")
+        self.date_time_stamp_display = self.date_time_stamp.strftime("%A, %d %B %Y %H:%M %Z")
         self.date_time.setText("Date & Time Stamp: {0}".format(self.date_time_stamp_display))
 
         self.medical_information_label = QLabel("Medical Information")
@@ -134,9 +134,12 @@ class AddMedicalInfo(QWidget):
     def edit_medical_information(self): 
         self.title_label = QLabel("""<html>
                                           <body>
-                                               <p><span style=" font-size:16pt; font-weight:1000; color:green">Search Customer</span></p>
+                                               <p><span style=" font-size:16pt; font-weight:1000; color:green">Search Medical Information</span></p>
                                           </body>
                                      </html>""")
+
+        self.customer_name_label = QLabel("Customer Name: {0} {1}".format(self.customer_details['FirstName'],
+                                                                            self.customer_details['LastName']))
 
         self.model = self.connection.find_medical_info_by_customer_id(self.customer_details['CustomerID'])
 
@@ -150,9 +153,83 @@ class AddMedicalInfo(QWidget):
         self.select_medical_information_widget = QWidget()
         self.select_medical_information_layout = QVBoxLayout()
         self.select_medical_information_layout.addWidget(self.title_label)
+        self.select_medical_information_layout.addWidget(self.customer_name_label)
         self.select_medical_information_layout.addWidget(self.medical_info)
         self.select_medical_information_layout.addWidget(self.select_medical_information_button)
         self.select_medical_information_widget.setLayout(self.select_medical_information_layout)
         
         self.stacked_layout.addWidget(self.select_medical_information_widget)
         self.stacked_layout.setCurrentIndex(2)
+
+        #connections 
+        self.select_medical_information_button.clicked.connect(self.edit_medical_information_information)
+
+    def edit_medical_information_information(self):
+        
+        self.index = self.medical_info.selectedIndexes()
+
+        self.title_label = QLabel("""<html>
+                                          <body>
+                                               <p><span style=" font-size:16pt; font-weight:1000;">Edit Medical Infomation</span></p>
+                                          </body>
+                                     </html>""")
+
+        self.error_label = QLabel()
+        self.error_label.hide()
+
+        self.customer_name = QLabel()
+        self.customer_name.setText("Customer Name: {0} {1}".format(self.customer_details['FirstName'],
+                                                                    self.customer_details['LastName']))
+
+        self.customer_address = QLabel()
+        self.customer_address.setText("Customer Address: {0}, {1}, {2}, {3}, {4}".format(self.customer_details['House'],
+                                                                                        self.customer_details['Road'],
+                                                                                        self.customer_details['City'],
+                                                                                        self.customer_details['County'],
+                                                                                        self.customer_details['Postcode']))
+
+        self.customer_contact = QLabel()
+        self.customer_contact.setText("Contact Details: {0}, {1}, {2}".format(self.customer_details['Home'],
+                                                                                self.customer_details['Mobile'],
+                                                                                self.customer_details['Email']))
+
+        self.created_on = QLabel("Medical Information Created on: {0}".format(self.medical_info.model().data(self.index[1])))
+
+        self.date_time = QLabel()
+        self.date_time_stamp = datetime.datetime.now()
+        #For Data Save
+        self.date_time_stamp_save = self.date_time_stamp.strftime("%Y-%m-%d %H:%M")
+        #For Display
+        self.date_time_stamp_display = self.date_time_stamp.strftime("%A, %d %B %Y %H:%M %Z")
+        self.date_time.setText("Date & Time Stamp: {0}".format(self.date_time_stamp_display))
+
+        self.medical_information_label = QLabel("Medical Information")
+        self.medical_information_ledit = QPlainTextEdit()
+        self.medical_information_ledit.setPlainText(self.medical_info.model().data(self.index[2]))
+
+        self.submit_button = QPushButton("Update Medical Information")
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.title_label)
+        self.layout.addWidget(self.error_label)
+        self.layout.addWidget(self.customer_name)
+        self.layout.addWidget(self.customer_address)
+        self.layout.addWidget(self.customer_contact)
+        self.layout.addWidget(self.created_on)
+        self.layout.addWidget(self.date_time)
+        self.layout.addWidget(self.medical_information_label)
+        self.layout.addWidget(self.medical_information_ledit)
+        self.layout.addWidget(self.submit_button)
+
+        self.add_medical_information_widget = QWidget()
+        self.add_medical_information_widget.setLayout(self.layout)
+
+        self.stacked_layout.addWidget(self.add_medical_information_widget)
+        self.stacked_layout.setCurrentIndex(3)
+
+        #connections
+        self.submit_button.clicked.connect(self.update_medical_information)
+
+    def update_medical_information(self):
+        self.medical_information_ledit = self.medical_information_ledit.toPlainText() + "\n Updated on {0}".format(self.date_time_stamp_display)
+        print(self.medical_information_ledit)
